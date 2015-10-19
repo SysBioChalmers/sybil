5b2ab491e801cb866a305630c78b5f264d682a99
Claus Jonathan Fritzemeier <clausjonathan.fritzemeier@hhu.de>
2015-10-19 16:31:19 +0200

    easyconstraint documentation and implementation

--- R/sysBiolAlg_fbaEasyConstraintClass.R
+++ R/sysBiolAlg_fbaEasyConstraintClass.R
@@ -100,32 +100,61 @@ setMethod(f = "initialize",
                   
                   mat <- S(model)
                   rtype <- rep("E", nRows)
+                  rlb <- rep(0, nRows)
+                  rub <- rep(0, nRows)
                   
                   #add easyConstraints:
                   if(!is.null(easyConstraint)){
-                  	if(		length(easyConstraint$nz) == length(easyConstraint$x)
-                  		| 	length(easyConstraint$nz) == length(easyConstraint$rtype)){
+                  	if(		length(easyConstraint$react) != length(easyConstraint$x)
+                  		| 	length(easyConstraint$react) != length(easyConstraint$rtype)
+                  		){
                   		stop("easyConstraint elements have to have equal lengths")
                   	}
-                  	stopifnot(is.list(easyConstraints$nz))
-                  	stopifnot(is.list(easyConstraints$x))
-                  	stopifnot(all(easyConstraints$rtype %in% c("F", "L", "U", "D", "E")))
+                  	stopifnot(is.list(easyConstraint$react))
+                  	stopifnot(is.list(easyConstraint$x))
+                  	stopifnot(all(easyConstraint$rtype %in% c("F", "L", "U", "D", "E")))
                   	
-                  	m <- Matrix(0, ncol=nCols, nrow=length(easyConstraint))
-                  	
-                  	for(i in 1:length(easyConstraint)){
-                  		m[i, easyConstraint$nz] <- easyConstraint$x
+                  	# setting and checking rlb
+                  	if(is.null(easyConstraint$lb)){
+                  		rlb <- c(rlb, rep(0, length(easyConstraint$react)))
+                  	}else{
+                  		if(length(easyConstraint$react) != length(easyConstraint$lb)){
+                  			stop("easyConstraint$lb length has to match length of react argument")
+                  		}else{
+                  			stopifnot(is.numeric(easyConstraint$lb))
+                  			rlb <- c(rlb, easyConstraint$lb)
+                  		}
                   	}
+                  	
+                  	# setting and checking rub
+                  	if(is.null(easyConstraint$ub)){
+                  		rub <- c(rub, rep(0, length(easyConstraint$react)))
+                  	}else{
+                  		if(length(easyConstraint$react) != length(easyConstraint$ub)){
+                  			stop("easyConstraint$ub length has to match length of react argument")
+                  		}else{
+                  			stopifnot(is.numeric(easyConstraint$ub))
+                  			rub <- c(rub, easyConstraint$ub)
+                  		}
+                  	}
+                  	browser()
+                  	
+                  	m <- Matrix(0, ncol=nCols, nrow=length(easyConstraint$react))
+                  	
+                  	for(i in 1:length(easyConstraint$react)){
+                  		m[i, easyConstraint$react[[i]]] <- easyConstraint$x[[i]]
+                  	}
+                  	
                   	
                   	mat <- rbind2(mat, m)
                   	rtype <- c(rtype, easyConstraint$rtype)
-                  	nRow <- nRow + length(easyConstraint)
+                  	nRows <- nRows + length(easyConstraint$react)
                   	if(!is.null(rowNames)){
-                  		c(rowNames, paste0("easyConstraint", 1:length(easyConstraint)))
+                  		c(rowNames, paste0("easyConstraint", 1:length(easyConstraint$react)))
                   	}
                   	
                   }
-
+                  
                   # generate problem object
                   .Object <- callNextMethod(.Object,
                                             sbalg      = "fba",
@@ -138,10 +167,10 @@ setMethod(f = "initialize",
                                             ub         = uppbnd(model),
                                             lb         = lowbnd(model),
                                             obj        = obj_coef(model),
-                                            rlb        = rep(0, nRows),
+                                            rlb        = rlb,
                                             rtype      = rtype,
                                             lpdir      = lpdir,
-                                            rub        = NULL,
+                                            rub        = rub,
                                             ctype      = NULL,
                                             cnames     = colNames,
                                             rnames     = rowNames,
